322f12e3d98310d1cdd1790f4d2b7729230881d8
ldenora <ldenora@codearchitects.com>
2022-05-21 17:42:49 +0200

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then((m) => m.BlogModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'salottino';
+  isAdminPage$: Observable<boolean> = of(false);
+  constructor(private _router: Router) {}
+  ngOnInit() {
+    this.isAdminPage$ = this._router.events.pipe(
+      filter((evt: any) => {
+        return evt instanceof NavigationEnd;
+      }),
+      map((evt: NavigationEnd) => {
+        return evt.url.includes('/admin');
+      })
+    );
+  }
 }
